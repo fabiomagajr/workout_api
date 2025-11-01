675a6ba0590a1c060cee28403267c05962b691d7
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2025-11-01 20:07:09 -0300

    Update AtletaUpdate schema and fix formatting
    Added newline at the end of the file and updated AtletaUpdate schema.

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,3 +1,5 @@
+# workout_api/atleta/schemas.py
+
 from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
 from workout_api.categorias.schemas import CategoriaIn
@@ -27,3 +29,10 @@ class AtletaOut(Atleta, OutMixin):
 class AtletaUpdate(BaseSchema):
     nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
     idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
+
+
+class AtletaListOut(BaseSchema):
+    """ Schema de resposta para listagem de atletas (nome, categoria, ct) """
+    nome: Annotated[str, Field(description='Nome do atleta', example='Joao', max_length=50)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
