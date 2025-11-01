db9041392b1ef344683391806c58601b6cb04d4e
Fabio Magalhães <112334507+fabiomagajr@users.noreply.github.com>
2025-11-01 20:06:14 -0300

    Add pagination support to the FastAPI app

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,5 +1,10 @@
+# workout_api/main.py
+
 from fastapi import FastAPI
 from workout_api.routers import api_router
+from fastapi_pagination import add_pagination  
 
 app = FastAPI(title='WorkoutApi')
 app.include_router(api_router)
+
+add_pagination(app)  
